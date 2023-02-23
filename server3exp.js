1dec46bf822f3e85fe45d473dee31abf1e0d5f68
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2023-02-23 19:39:13 +0530

    Files Added

--- server3exp.js
+++ server3exp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var apth = require('path');
+var path = require('path');
+var fs = require('fs');
 var port = 3000;
 
 // Creating Server
@@ -11,6 +12,8 @@ app.listen(port, function () {
 // Including Static Files
 app.use('/cssFiles', express.static(__dirname + '/styles'))
 // http://localhost:3000/cssFiles/styles.css ==> will show my css code
+
+// app.use('/files', express.static(__dirname + '/files'))
 
 
 // Creating Homepage Route
@@ -27,6 +30,20 @@ app.get('/', function (req, res) {
 // Creating Routes
 app.get('/rumi', function (req, res) {
     res.send('Hi rumi');
+});
+
+// Regex Routers (for dynamic routes) starts and ends with anything
+app.get(/^(.+)$/, function (req, res) {
+    try {
+        if (fs.statSync(path.join(__dirname, './files/', req.params[0] + '.html')).isFile()) {
+            console.log('File Exists');
+            res.sendFile(req.params[0] + '.html', { root: path.join(__dirname, './files') });
+        }
+    }
+    catch (err) {
+        console.log('File Does Not Exists');
+        res.sendFile(__dirname + '/files/404.html');
+    }
 });
 
 
